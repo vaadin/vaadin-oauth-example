25f3e9df8ff01672d9bfed366b78cd98924f626d
thomas <thomas@vaadin.com>
2020-09-03 15:50:11 +0300

    Adds message about missing OAuth config

--- src/main/java/com/vaadin/example/oauth/ui/LoginScreen.java
+++ src/main/java/com/vaadin/example/oauth/ui/LoginScreen.java
@@ -1,6 +1,11 @@
 package com.vaadin.example.oauth.ui;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -17,13 +22,31 @@ public class LoginScreen extends VerticalLayout {
      */
     private static final String URL = "/oauth2/authorization/google";
 
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientkey;
+
     public LoginScreen() {
 
         setPadding(true);
         setAlignItems(Alignment.CENTER);
+    }
 
-        Anchor gplusLoginButton = new Anchor(URL, "Login with Google");
-        gplusLoginButton.getStyle().set("margin-top", "100px");
-        add(gplusLoginButton);
+    @PostConstruct
+    public void initView() {
+
+        // Check that oauth keys are present
+        if (clientkey == null || clientkey.isEmpty() || clientkey.length() < 16) {
+            Paragraph text = new Paragraph("Could not find OAuth client key in application.properties. "
+                    + "Please double-check the key and refer to the README.md file for instructions.");
+            text.getStyle().set("padding-top", "100px");
+            add(text);
+
+        } else {
+
+            Anchor gplusLoginButton = new Anchor(URL, "Login with Google");
+            gplusLoginButton.getStyle().set("margin-top", "100px");
+            add(gplusLoginButton);
+        }
+
     }
 }
