09da5760e9558b7e658660c48f16922cf6486ef7
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-03-21 10:56:40 +0200

    Use VaadinWebSecurityConfigurerAdapter

--- src/main/java/com/vaadin/example/oauth/ui/MainView.java
+++ src/main/java/com/vaadin/example/oauth/ui/MainView.java
@@ -1,44 +1,46 @@
 
 package com.vaadin.example.oauth.ui;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.example.oauth.data.UserSession;
-import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServletRequest;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import javax.annotation.security.PermitAll;
 
 /**
  * Application main class that is hidden to user before authentication.
+ *
+ * The class is annotated with {@code @PermitAll} to allow only authenticated
+ * users to view the class.
  */
 @Route("")
+@PermitAll
 public class MainView extends VerticalLayout {
 
-    @Autowired
-    UserSession userSession;
+    private static final String LOGOUT_SUCCESS_URL = "/";
 
-    /**
-     * After user has authenticated with Google login and has granted access to the
-     * application on the Authorize application page, will see the information.
-     */
-    @PostConstruct
-    public void init() {
-
+    public MainView(UserSession userSession) {
         Div div = new Div();
         div.setText("Hello " + userSession.getUser().getFirstName() + " " + userSession.getUser().getLastName());
         div.getElement().getStyle().set("font-size", "xx-large");
 
         Image image = new Image(userSession.getUser().getPicture(), "User Image");
 
-        // Spring maps the 'logout' url so we should ignore it
-        Anchor logout = new Anchor("/logout", "Logout");
-        logout.getElement().setAttribute("router-ignore", true);
+        Button logoutButton = new Button("Logout", click -> {
+            UI.getCurrent().getPage().setLocation(LOGOUT_SUCCESS_URL);
+            SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+            logoutHandler.logout(
+                    VaadinServletRequest.getCurrent().getHttpServletRequest(), null,
+                    null);
+        });
 
         setAlignItems(Alignment.CENTER);
-        add(div, image, logout);
+        add(div, image, logoutButton);
     }
 }
