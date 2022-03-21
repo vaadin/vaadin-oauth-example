1fdbce0b9da2cd665e192b8a9339847a750fce51
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-03-21 10:57:32 +0200

    Merge pull request #27 from vaadin/VaadinWebSecurityConfigurerAdapter
    Use VaadinWebSecurityConfigurerAdapter

--- src/main/java/com/vaadin/example/SecurityConfiguration.java
+++ src/main/java/com/vaadin/example/SecurityConfiguration.java
@@ -1,85 +1,33 @@
 package com.vaadin.example;
 
-import java.util.stream.Stream;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.vaadin.flow.server.HandlerHelper;
-import com.vaadin.flow.shared.ApplicationConstants;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 /**
- * Configures Spring Security, doing the following:
- * <li>Bypass security checks for static resources,</li>
- * <li>Restrict access to the application, allowing only logged in users,</li>
- * <li>Set up the login form,</li>
+ * Configures Spring Security using VaadinWebSecurityConfigurerAdapter helper.
+ * <br><br>
+ *
+ * VaadinWebSecurityConfigurerAdapter provides basic Vaadin security
+ * configuration for the project out of the box. It sets up security rules for a
+ * Vaadin application and restricts all URLs except for public resources and
+ * internal Vaadin URLs to authenticated user.<br><br>
+ *
+ * In this class, we only need to alter the {@code HttpSecurity}
+ * configuration in order to configure authentication support using an OAuth 2.0.
  */
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     private static final String LOGIN_URL = "/login";
-    private static final String LOGOUT_URL = "/logout";
-    private static final String LOGOUT_SUCCESS_URL = "/";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-
-                // Allow all flow internal requests.
-                .authorizeRequests().requestMatchers(SecurityConfiguration::isFrameworkInternalRequest).permitAll()
-
-                // Restrict access to our application.
-                .and().authorizeRequests().anyRequest().authenticated()
-
-                // Not using Spring CSRF here to be able to use plain HTML for the login page
-                .and().csrf().disable()
-
-                // Configure logout
-                .logout().logoutUrl(LOGOUT_URL).logoutSuccessUrl(LOGOUT_SUCCESS_URL)
-
-                // Configure the login page with OAuth.
-                .and().oauth2Login().loginPage(LOGIN_URL).permitAll();
+        super.configure(http);
+        http.oauth2Login().loginPage(LOGIN_URL).permitAll();
     }
 
-    /**
-     * Allows access to static resources, bypassing Spring Security.
-     */
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(
-                // client-side JS code
-                "/VAADIN/**",
-
-                // client-side JS code
-                "/frontend/**",
-
-                // the standard favicon URI
-                "/favicon.ico",
-
-                // web application manifest
-                "/manifest.webmanifest", "/sw.js", "/offline-page.html",
-
-                // icons and images
-                "/icons/**", "/images/**");
-    }
-
-    /**
-     * Tests if the request is an internal framework request. The test consists of
-     * checking if the request parameter is present and if its value is consistent
-     * with any of the request types know.
-     *
-     * @param request {@link HttpServletRequest}
-     * @return true if is an internal framework request. False otherwise.
-     */
-    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
-        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
-        return parameterValue != null && Stream.of(HandlerHelper.RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
-    }
 }
--- src/main/java/com/vaadin/example/oauth/ui/LoginView.java
+++ src/main/java/com/vaadin/example/oauth/ui/LoginView.java
@@ -2,6 +2,7 @@ package com.vaadin.example.oauth.ui;
 
 import javax.annotation.PostConstruct;
 
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.vaadin.flow.component.html.Anchor;
@@ -11,22 +12,25 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 /**
- * Adds an explicit link that the user has to click to login.
+ * Adds a link that the user has to click to login.
+ *
+ * This view is marked with {@code @AnonymousAllowed} to allow all users access
+ * to the login page.
  */
 @Route("login")
 @PageTitle("Login")
-public class LoginScreen extends VerticalLayout {
+@AnonymousAllowed
+public class LoginView extends VerticalLayout {
 
     /**
      * URL that Spring uses to connect to Google services
      */
-    private static final String URL = "/oauth2/authorization/google";
+    private static final String OAUTH_URL = "/oauth2/authorization/google";
 
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
     private String clientkey;
 
-    public LoginScreen() {
-
+    public LoginView() {
         setPadding(true);
         setAlignItems(Alignment.CENTER);
     }
@@ -40,12 +44,12 @@ public class LoginScreen extends VerticalLayout {
                     + "Please double-check the key and refer to the README.md file for instructions.");
             text.getStyle().set("padding-top", "100px");
             add(text);
-
         } else {
-            Anchor gplusLoginButton = new Anchor(URL, "Login with Google");
-            gplusLoginButton.getElement().setAttribute("router-ignore", true);
-            gplusLoginButton.getStyle().set("margin-top", "100px");
-            add(gplusLoginButton);
+            Anchor loginLink = new Anchor(OAUTH_URL, "Login with Google");
+            // Set router-ignore attribute so that Vaadin router doesn't handle the login request
+            loginLink.getElement().setAttribute("router-ignore", true);
+            loginLink.getStyle().set("margin-top", "100px");
+            add(loginLink);
         }
 
     }
